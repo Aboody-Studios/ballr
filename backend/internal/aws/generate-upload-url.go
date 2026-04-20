bcf75719944356d2a93dce97dccfa9999bdc816a
Zero <188656344+k5602@users.noreply.github.com>
2026-04-21 01:22:33 +0200

    feat(video): add upload URL handler with format and size validation

--- backend/internal/aws/generate-upload-url.go
+++
@@ -1,8 +0,0 @@
-package aws
-
-import "github.com/Aboody-Studios/ballr/backend/internal/models"
-
-func GenerateUploadURL(video models.Video) (string, error) {
-	// generate s3 upload url here when we have an amazon account
-	return "lol", nil
-}
